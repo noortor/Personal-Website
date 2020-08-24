6b8ecda4e07198bcbd7dec5c9faa1ba5f4f688aa
Noor Mahini <nmahini@berkeley.edu>
2020-08-23 17:59:20 -0700

    new working parallax

--- src/motion.js
+++ src/motion.js
@@ -1,4 +1,4 @@
-  $(window).scroll(function(){
-    var scrollval = $(this).scrollTop();    // It will return scroll value
-    $("#header-content").css("transform",'translate(0px,-'+scrollval/20+'%)');
+$(window).scroll(function () {
+  var scrollval = $(this).scrollTop(); // It will return scroll value
+  $(".header-img").css("transform", "translate(0px,-" + scrollval / 20 + "%)");
 });
