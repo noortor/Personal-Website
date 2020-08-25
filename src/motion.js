c0b94419cbccfba40bb7be6df2c2f2d30ce76612
Noor Mahini <nmahini@berkeley.edu>
2020-08-24 18:04:45 -0700

    truly removed src

--- src/motion.js
+++
@@ -1,4 +0,0 @@
-$(window).scroll(function () {
-  var scrollval = $(this).scrollTop(); // It will return scroll value
-  $(".header-img").css("transform", "translate(0px,-" + scrollval / 20 + "%)");
-});
